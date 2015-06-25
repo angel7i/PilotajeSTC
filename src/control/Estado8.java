6f9a54907555a86b905ee314f2057c4beeaa5db2
Angel <plussnoise@gmail.com>
2015-06-25 16:07:54 -0500

    Prototipo

---
+++ src/control/Estado8.java
@@ -0,0 +1,73 @@
+package control;
+
+import tables8.Actual;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/estado8")
+public class Estado8 extends HttpServlet implements Servlet
+{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        try
+        {
+            int info = Integer.parseInt(req.getParameter("estacion"));
+            Actual a = Linea8DAO.getActualizacion(info);
+
+            if (a == null)
+            {
+                System.out.println("A null");
+                JsonObject e = Json.createObjectBuilder().
+                        add("error", "ErrorDB").build();
+                resp.getWriter().print(e);
+                return;
+            }
+
+            JsonObject object = Json.createObjectBuilder().
+                    add("vif1", a.getVif1()).
+                    add("vif2", a.getVif2()).
+                    add("vcor", a.getVcor()).
+                    add("npc", a.getNpc()).
+                    add("pfa1", a.getPfa1()).
+                    add("pfa2", a.getPfa2()).
+                    add("pfa3", a.getPfa3()).
+                    add("pfa4", a.getPfa4()).
+                    add("fecha", fecha(a.getFecha())).build();
+
+            resp.getWriter().print(object);
+        }
+        catch (NullPointerException npe)
+        {
+            System.out.println("NUllPoinExcp null");
+            resp.getWriter().print("ErrorBD");
+            npe.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        doPost(req, resp);
+    }
+
+    private String fecha(Date d)
+    {
+        SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss a'   'dd/MM/yyyy");
+        return format.format(d);
+    }
+}
---
+++ src/control/Linea8DAO.java
@@ -0,0 +1,181 @@
+package control;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ManagedSessionContext;
+import org.hibernate.exception.GenericJDBCException;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.UnknownServiceException;
+import tables8.Actual;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Linea8DAO
+{
+    private static String database;
+    private static String db;
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    public static String config(int sta)
+    {
+        switch (sta)
+        {
+            case 1:
+                return "Iztac.cfg.xml";
+            case 2:
+                return "Chabac.cfg.xml";
+            case 3:
+                return "Garibal8.cfg.xml";
+            case 4:
+                return "Obrera.cfg.xml";
+            case 5:
+                return "Iztapa.cfg.xml";
+            case 6:
+                return "LaViga.cfg.xml";
+            case 7:
+                return "Consti.cfg.xml";
+            case 8:
+                return "UAM.cfg.xml";
+            case 9:
+                return "CerEst.cfg.xml";
+            case 10:
+                return "Escuad.cfg.xml";
+            case 11:
+                return "SalAgu.cfg.xml";
+            case 12:
+                return "BelArt.cfg.xml";
+            case 13:
+                return "SJuanL.cfg.xml";
+            case 14:
+                return "Aculco.cfg.xml";
+            case 15:
+                return "Apatlaco.cfg.xml";
+            case 16:
+                return "Coyuya.cfg.xml";
+            case 17:
+                return "Doctor.cfg.xml";
+            case 18:
+                return "Atlalilco.cfg.xml";
+            case 19:
+                return "SAnita.cfg.xml";
+        }
+
+        return "";
+    }
+
+    private static SessionFactory buildSessionFactory()
+    {
+        try
+        {
+            Configuration configuration = new Configuration();
+            configuration.configure(database);
+
+            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            return sessionFactory;
+        }
+        catch (Throwable ex)
+        {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(int sta)
+    {
+        try
+        {
+            if (sessionFactory == null)
+            {
+                System.out.println("Load " + database);
+                sessionFactory = buildSessionFactory();
+            }
+            else
+            {
+                System.out.println(database + " = " + db);
+                if (!Objects.equals(database, db))
+                {
+                    System.out.println("Change " + database);
+                    sessionFactory.close();
+                    sessionFactory = buildSessionFactory();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error Build");
+            e.printStackTrace();
+        }
+
+        return sessionFactory;
+    }
+
+    public static Actual getActualizacion(int sta)
+    {
+        db = database;
+        database = config(sta);
+        List<Actual> la = null;
+        Session session = null;
+        Transaction tr = null;
+
+        try
+        {
+            session = getSessionFactory(sta).openSession();
+
+            ManagedSessionContext.bind(session);
+            tr = session.getTransaction();
+        }
+        catch (Exception e)
+        {
+            if (e instanceof UnknownServiceException)
+            {
+                sessionFactory.close();
+                return null;
+            }
+            else
+            {
+                System.out.println("Error de Session");
+                e.printStackTrace();
+            }
+        }
+
+
+        try
+        {
+            tr.begin();
+            la = session.createCriteria(Actual.class).list();
+
+            if (!tr.wasCommitted())
+                tr.commit();
+        }
+        catch (GenericJDBCException ge)
+        {
+            System.out.println("Error1");
+            sessionFactory.close();
+//            ge.printStackTrace();
+
+            return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error2");
+            e.printStackTrace();
+
+            return null;
+        }
+        finally
+        {
+            if (session != null && (session.isConnected() || session.isOpen()))
+                session.close();
+            ManagedSessionContext.unbind(sessionFactory);
+        }
+
+        return la.get(0);
+    }
+}
